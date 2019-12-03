58d8467436dd96b16cfd625878ab80a6f973d264
pkarkazis <pkarkazis@synelixis.com>
2019-12-03 22:55:54 +0000

    add monitoring endpoints functionality

--- src/tnglib/monitor.py
+++ src/tnglib/monitor.py
@@ -66,7 +66,7 @@ def add_prometheus_targets(name, endpoint, type, path):
 
     if type == 'k8s':
         trg = {'honor_labels':True,'job_name':name,'metrics_path':path,
-               'params':{'match'[]:["{job='kubernetes-cadvisor'}",
+               'params':{'match[]':["{job='kubernetes-cadvisor'}",
                                   "{job='kubernetes-nodes'}",
                                   "{job='kubernetes-pods'}",
                                   "{job='pushgateway'}"]},
